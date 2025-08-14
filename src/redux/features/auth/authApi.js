8f5cacb51c774ea7c5fd5a5951dd9396b4f07ae2
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-08-14 15:48:46 +0600

    auth api created

--- src/redux/features/auth/authApi.js
+++ src/redux/features/auth/authApi.js
@@ -1,5 +1,22 @@
 import { apiSlice } from "../api/apiSlice";
 
 export const authApi = apiSlice.injectEndpoints({
-  endpoints: (build = {}),
+  endpoints: (build) => ({
+    register: build.mutation({
+      query: (data) => ({
+        url: "/register",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    login: build.mutation({
+      query: (data) => ({
+        url: "/login",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
 });
+
+export const { useRegisterMutation, useLoginMutation } = authApi;
