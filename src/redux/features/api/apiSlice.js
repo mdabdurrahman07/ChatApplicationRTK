67e706935fee26131da8bf2862f987d9c4f2c59b
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-08-14 15:30:32 +0600

    redux features api boiler template setup and store configration

--- src/redux/features/api/apiSlice.js
+++ src/redux/features/api/apiSlice.js
@@ -1,6 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 export const apiSlice = createApi({
-    reducerPath: "chatApi",
-    baseQuery: fetchBaseQuery({baseUrl:''})
-})
+  reducerPath: "chatApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_SERVER_API_URL,
+  }),
+  tagTypes: [],
+  endpoints: (build) => ({}),
+});
