5070eb66eceb8842acbb2d980dc0037ce98212ff
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-08-18 00:17:40 +0600

    authentication implemented on root

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { useAuthCheck } from "../hooks/useAuthCheck";
+import Loading from "../components/Loading/Loading";
 
 const Root = () => {
-    return (
-        <div>
-            <Outlet/>
-        </div>
-    );
+  const auth = useAuthCheck();
+  return !auth ? (
+    <Loading />
+  ) : (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default Root;
