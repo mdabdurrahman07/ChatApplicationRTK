c0333f8773386fffe443f67ddddc4e36bf7f71fd
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-08-18 22:36:28 +0600

    user logout functionality added

--- src/redux/features/api/apiSlice.js
+++ src/redux/features/api/apiSlice.js
@@ -5,8 +5,6 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_SERVER_API_URL,
     prepareHeaders: async (Headers, { getState, endpoint }) => {
-      console.log("checking what is getState in baseApi", getState);
-      console.log("checking what is endpoints in baseApi", endpoint);
       const token = getState()?.auth.accessToken;
       if (token) {
         Headers.set("Authorization", `Bearer ${token}`);
