a272403153a45002a34f8c9ceefdf4ca5cb6481b
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-08-24 21:52:34 +0600

    sidebar participant destructure

--- src/components/inbox/ChatIItems.jsx
+++ src/components/inbox/ChatIItems.jsx
@@ -1,28 +1,42 @@
+import { useSelector } from "react-redux";
+import { useGetConversationsQuery } from "../../redux/features/conversation/conversationApi";
 import ChatItem from "./ChatItem";
+import toast from "react-hot-toast";
+import moment from "moment";
+import getPartnerInfo from "../../utils/getPartnerInfo";
 
 export default function ChatItems() {
-    return (
-        <ul>
-            <li>
-                <ChatItem
-                    avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-                    name="Saad Hasan"
-                    lastMessage="bye"
-                    lastTime="25 minutes"
-                />
-                <ChatItem
-                    avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-                    name="Sumit Saha"
-                    lastMessage="will talk to you later"
-                    lastTime="10 minutes"
-                />
-                <ChatItem
-                    avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-                    name="Mehedi Hasan"
-                    lastMessage="thanks for your support"
-                    lastTime="15 minutes"
-                />
-            </li>
-        </ul>
-    );
+  const { user } = useSelector((state) => state.auth) || {};
+  const { email } = user || {};
+  //   what to render
+  const {
+    data: conversations,
+    isError,
+    error,
+    isLoading,
+  } = useGetConversationsQuery(email);
+  let content = null;
+  if (isLoading) {
+    content = <li className="m-2 text-center">Loading...</li>;
+  } else if (!isLoading && isError) {
+    content = toast.error(error);
+  } else if (!isLoading && !isError && conversations.length === 0) {
+    content = toast.error("No conversation found");
+  } else {
+    content = conversations.map((conversation) => {
+      const { id, message, timestamp } = conversation;
+      const {name} = getPartnerInfo(conversation.users , email)
+      return (
+        <li key={id}>
+          <ChatItem
+            avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
+            name={name}
+            lastMessage={message}
+            lastTime={moment(timestamp).fromNow()}
+          />
+        </li>
+      );
+    });
+  }
+  return <ul>{content}</ul>;
 }
