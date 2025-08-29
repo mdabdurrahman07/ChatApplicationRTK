def13447a69cc0beb173aef5823d0589b4410097
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-08-29 18:25:55 +0600

    messages api updated

--- src/redux/features/messages/messagesApi.js
+++ src/redux/features/messages/messagesApi.js
@@ -1,5 +1,12 @@
 import { apiSlice } from "../api/apiSlice";
 
 export const messagesApi = apiSlice.injectEndpoints({
-  endpoints: (build = {}),
+  endpoints: (build) => ({
+    getMessages: build.query({
+      query: (id) =>
+        `/messages?conversationId_like=${id}&_sort=timestamp&_order=desc&_page=1&_limit=5`,
+    }),
+  }),
 });
+
+export const { useGetMessagesQuery } = messagesApi;
